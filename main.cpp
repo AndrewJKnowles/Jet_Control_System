6351a165a00fe905d48c332c00c7859e950eff23
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2023-02-22 18:39:33 +0000

    serial

--- main.cpp
+++ main.cpp
@@ -2,39 +2,48 @@
 #include "MD10C.h"
 #include "Parallax_360.h"
 
-//          (dir, pwm, anlog In)
-MD10C motor(p19, p21, p20);
-Parallax_360 servo(p22, p23);
-InterruptIn button(p18);    //artificial limit switch
+BufferedSerial pc(USBTX, USBRX);
 
-void interrupt_isr();       //service routine
+char *input = new char[1];
 
-int direction = 1;          //initial motor direction (clockwise)
-volatile int g_button_flag = 0; 
+char mainMenu[] = "Select an option:\n 1. Extrude Material\n 2. Retract Bed\n 3. Retract Actuator\n 4. System Reset\n 5. Refill Hopper\n 6. Test Actuator\n 7. Test Servo\n";
+char message1[] = "Option 1";
+char message2[] = "Option 2";
+char message3[] = "Option 3";
+char message4[] = "Option 4";
+char message5[] = "Option 5";
+char message6[] = "Option 6";
+char message7[] = "Option 7";
 
 int main(){
-    button.rise(&interrupt_isr);    //set isr condition
-    button.mode(PullNone);          //set button mode
-    motor.init();                   //initialise motor driver
-    servo.init();
+    pc.set_baud(9600);
+    pc.write(mainMenu, sizeof(mainMenu));
+    while(1){
+        pc.read(input, sizeof(input));
 
+        if(*input == '1'){
+            pc.write(message1, sizeof(message1));
 
-    /*while(1){
-        if(g_button_flag == 1){     //if limit switch active
-            g_button_flag = 0;      //clear flag
-            direction = !direction; //change direction status
-            motor.stop();           //stop motor
-            wait_us(500000);        //wait for complete stop
+        }else if(*input == '2'){
+            pc.write(message2, sizeof(message2));
+
+        }else if(*input == '3'){
+            pc.write(message3, sizeof(message3));
+
+        }else if(*input == '4'){
+            pc.write(message4, sizeof(message4));
+
+        }else if(*input == '5'){
+            pc.write(message5, sizeof(message5));
+
+        }else if(*input == '6'){
+            pc.write(message6, sizeof(message6));
+
+        }else if(*input == '7'){
+            pc.write(message7, sizeof(message7));
+
+        }else{
+            printf(">> error invalid option");
         }
-
-        motor.motorOn(direction);   //begin motor
-    };*/
-
-    servo.test();
-
-    while(1){}
-}
-
-void interrupt_isr(){
-    g_button_flag = 1;
+    }
 }
