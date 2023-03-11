7e9a17f018b7233ff691f3b79cf9f19eba03bdc0
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2023-03-11 16:52:30 +0000

    Sub Menu bug fix

--- main.cpp
+++ main.cpp
@@ -11,17 +11,20 @@ Parallax_360 servo(PB_7, PC_13);   //for nucleo use
 
 BufferedSerial pc(USBTX, USBRX);        //establish serial communications between PC and NUCLEO
 
-char mainMenu[] = "Main Menu\n a. Extrude Material\n b. Fill Hopper\n c. Retract Actuator\n d. Retract Servo\n e. Manual Operation\n f. Set Duty Cyle\n";
-char retractingActuator[] = " Retracting Actuator...   To stop Press 'X'\n ";
-char retractionStopped[] =  "Retraction Stopped\n";
-char manual[] = "Manual mode: Ensure Pot is initially set to 0\n Press 'Y' to confirm\n";
-char manual2[] = "Entering manual mode:\n Use 'E' to extend and 'R' to retract the actuator\n Press 'X' to exit\n";
-char manual3[] = "Exiting manual mode\n";
-char error1[] = "Error: Invalid selection\n";
-
-char general[] = " In Loop\n";
+char mainMenu[] = "Main Menu\n a. Extrude Material\n b. Fill Hopper\n c. Retract Actuator\n d. Retract Servo\n e. Manual Operation\n f. Set Duty Cyle\n\n";
+char retractingActuator[] = " Retracting Actuator...   To stop Press 'X'\n\n";
+char retractionStopped[] =  "Retraction Stopped\n\n";
+char manual[] = "Manual mode: Ensure Pot is initially set to 0\n Press 'Y' to confirm\n\n";
+char manual2[] = "Entering manual mode:\n Use 'E' to extend\n Use 'R' to retract the actuator\n Use 'P' to pause actuator motion\n Press 'X' to exit\n\n";
+char manual3[] = "Exiting manual mode...\n\n";
+char manual4[] = "Extending Actuator...\n\n";
+char manual5[] = "Retracting actuator...\n\n";
+char manual6[] = "Actuation Paused\n\n";
+char error1[] = "Error: Invalid selection\n\n";
 
 char *input = new char[1];
+
+bool inputConfirmation = false;
 
 void initComms();
 void retractActuator();
@@ -70,56 +73,78 @@ int main(){
     }
 }
 
+//init comms protocols
 void initComms(){
     pc.set_baud(9600);
     pc.set_format(8, BufferedSerial::None, 1); //8-N-1
 }
 
+//option A
+
+//option B
+
+//option C
 void retractActuator(){
     pc.write(retractingActuator, sizeof(retractingActuator));   //general message output
 
-    do{
-        pc.read(input, sizeof(input));      //read input
-        motor.motorOn(RETRACTION, 0.10);    //turn actuator on with 10% duty cycle
-        ThisThread::sleep_for(20ms);
+    while(inputConfirmation == false){          //while input != x or X
+        motor.motorOn(RETRACTION, 0.10);        //turn actuator on with 10% duty cycle
+        pc.read(input, sizeof(input));          //read input
 
-        pc.write(general, sizeof(general)); //debug statement
+        if(*input == 'x' || *input == 'X'){     //check for valid exit character
+            inputConfirmation = true;           //set valid input to true
+        }
+    }
 
-    }while(*input != 'x' || *input != 'X'); //retract actuator while stop command is not given
-
-    pc.write(retractionStopped, sizeof(retractionStopped));
-    motor.stop();
+    inputConfirmation = false;                  //reset input flag
+    pc.write(retractionStopped, sizeof(retractionStopped)); //print message
+    motor.stop();                               //stop actuation
+    ThisThread::sleep_for(500ms);               
 }
 
+//option D
+
+//option E
 void manualOperation(){
-    pc.write(manual, sizeof(manual));
+    pc.write(manual, sizeof(manual));           //print general message
 
     //wait for confirmation that pot is set to 0
-    do{
-        pc.read(input, sizeof(input));
-        ThisThread::sleep_for(20ms);
-    }while(*input != 'y' || *input != 'Y');
+    while(inputConfirmation == false){          //while input != x or X
+        pc.read(input, sizeof(input));          //read input
 
-    pc.write(manual2, sizeof(manual2));
+        if(*input != 'y' || *input != 'Y'){     //check for valid exit character
+            inputConfirmation = true;           //set valid input to true
+        }
+    }
 
-    //allow manual operation
-    do{
-        pc.read(input, sizeof(input));
+    inputConfirmation = false;                  //reset input flag
+    pc.write(manual2, sizeof(manual2));         //print general message
 
-        if(*input == 'r' || *input == 'R'){
+    while(inputConfirmation == false){          //while input != x or X
+        pc.read(input, sizeof(input));          //read input
+
+        if(*input == 'r' || *input == 'R'){     //if input == r, retract actuator
+            pc.write(manual5, sizeof(manual5)); //print general message
             motor.manualMode(RETRACTION);
 
-        }else if(*input == 'e' || *input == 'E'){
+        }else if(*input == 'e' || *input == 'E'){   //if input == e, extend actuator
+            pc.write(manual4, sizeof(manual4));     //print general message
             motor.manualMode(EXTENSION);
 
-        }else{
-            pc.write(error1, sizeof(error1));
+        }else if(*input == 'p' || *input == 'P'){   //if input == p, pause actuator motion
+            pc.write(manual6, sizeof(manual6));     //print general message
+            motor.stop();
 
+        }else if(*input == 'x' || *input == 'X'){   //check for valid exit character
+            inputConfirmation = true;               //set valid input to true
         }
+    }
 
-        ThisThread::sleep_for(20ms);
-    }while(*input != 'x' || *input != 'X');
+    inputConfirmation = false;                      //reset input flag
+    pc.write(manual3, sizeof(manual3));             //print general message
+    motor.stop();                                   //stop motor
 
-    pc.write(manual3, sizeof(manual3));
-    motor.stop();
+    ThisThread::sleep_for(500ms);
 }
+
+//option F
